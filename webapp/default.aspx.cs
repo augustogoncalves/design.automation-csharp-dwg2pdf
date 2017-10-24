2039ce797555d124c71ae6f7862edd7b1b68f09b
Augusto Goncalves <augusto.goncalves@autodesk.com>
2017-10-24 13:53:16 -0200

    initial setup

---
+++ webapp/default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.S3;
+using System;
+
+namespace webapp
+{
+  public partial class _default : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      IAmazonS3 client = new AmazonS3Client(Amazon.RegionEndpoint.EUWest1);
+      var list = client.ListBuckets();
+    }
+  }
+}
