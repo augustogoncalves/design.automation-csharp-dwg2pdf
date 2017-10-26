43b1e51491f238d5e354c86d6a84d984e830a629
Augusto Goncalves <augusto.goncalves@autodesk.com>
2017-10-26 09:27:30 -0200

    AWS S3 with headers

--- webapp/App_Code/Config.cs
+++ webapp/App_Code/Config.cs
@@ -1,8 +1,22 @@
-﻿using Autodesk.Forge;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using Autodesk.Forge;
 using System.Web.Configuration;
 
 namespace webapp
@@ -18,6 +32,10 @@ namespace webapp
     /// The client secret of this app
     /// </summary>
     internal static string FORGE_CLIENT_SECRET { get { return GetAppSetting("FORGE_CLIENT_SECRET"); } }
+
+    internal static string AWS_ACCESS_KEY { get { return GetAppSetting("AWSAccessKey"); } }
+
+    internal static string AWS_SECRET_KEY { get { return GetAppSetting("AWSSecretKey"); } }
 
     /// <summary>
     /// List of scopes for Design Automation
--- webapp/default.aspx.cs
+++ webapp/default.aspx.cs
@@ -1,19 +1,34 @@
 ﻿using Amazon.S3;
-using Amazon.S3.Model;
 using Autodesk.Forge;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using System.Net;
 using System.Web;
 
 namespace webapp
 {
   public partial class _default : System.Web.UI.Page
   {
-    private const string BUCKET_NAME = "designautomation-sample";
-
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -21,6 +36,8 @@ namespace webapp
 
     protected async void Button1_Click(object sender, EventArgs e)
     {
+      // define the Bucket, DWG and PDF file names
+      string bucketName = "designautomationsample-" + Guid.NewGuid().ToString();
       string dwgFileName = FileUpload1.FileName;
       string pdfFileName = dwgFileName.Replace(".dwg", ".pdf");
 
@@ -29,31 +46,32 @@ namespace webapp
       Directory.CreateDirectory(Path.GetDirectoryName(fileSavePath));
       FileUpload1.SaveAs(fileSavePath);
 
+      IAmazonS3 client = new AmazonS3Client(Amazon.RegionEndpoint.USWest2);
+
       // create AWS Bucket
-      IAmazonS3 client = new AmazonS3Client(Amazon.RegionEndpoint.EUWest1);
+      if (!await client.DoesS3BucketExistAsync(bucketName))
+        await client.EnsureBucketExistsAsync(bucketName);
 
-      if (!await client.DoesS3BucketExistAsync(BUCKET_NAME))
-        await client.EnsureBucketExistsAsync(BUCKET_NAME);
+      // Upload file from Server to S3 bucket
+      client.UploadObjectFromFilePath(bucketName, FileUpload1.FileName, fileSavePath, null);
 
-      // Upload file to S3 bucket
-      client.UploadObjectFromFilePath(BUCKET_NAME, FileUpload1.FileName, fileSavePath, null);
-
-      // delete server files
+      // delete files from server
       Directory.Delete(Path.GetDirectoryName(fileSavePath), true);
 
       // OAuht 2-legged on Forge
       TwoLeggedApi apiInstance = new TwoLeggedApi();
       dynamic bearer = await apiInstance.AuthenticateAsync(Config.FORGE_CLIENT_ID, Config.FORGE_CLIENT_SECRET, Autodesk.Forge.oAuthConstants.CLIENT_CREDENTIALS, Config.FORGE_SCOPE_DESIGN_AUTOMATION);
 
-      var download = client.GeneratePreSignedURL(BUCKET_NAME, dwgFileName, DateTime.Now.AddMinutes(10), null);
+      // generate URLs for Design Automation to access (download & upload) S3 files
+      Uri downloadFromS3 = new Uri(client.GeneratePreSignedURL(bucketName, dwgFileName, DateTime.Now.AddMinutes(90), null));
+
       Dictionary<string, object> props = new Dictionary<string, object>();
       props.Add("Verb", "PUT");
-      var upload = client.GeneratePreSignedURL(BUCKET_NAME, pdfFileName, DateTime.Now.AddMinutes(10), props);
+      Uri uploadToS3 = new Uri(client.GeneratePreSignedURL(bucketName, pdfFileName, DateTime.Now.AddMinutes(10), props));
 
-      // prepare Activity
+      // prepare WorkItem (based on the built-in "PlotToPDF" activity")
       WorkItemsApi workItemsApi = new WorkItemsApi();
       workItemsApi.Configuration.AccessToken = bearer.access_token;
-
       JObject arguments = new JObject
       {
         new JProperty(
@@ -61,7 +79,8 @@ namespace webapp
           {
             new JObject
             {
-              new JProperty("Resource", download),
+              new JProperty("Resource", downloadFromS3.GetLeftPart(UriPartial.Path)),
+              new JProperty("Headers", MakeHeaders( WebRequestMethods.Http.Get, downloadFromS3)),
               new JProperty("Name",  "HostDwg")
             }
           }
@@ -73,24 +92,27 @@ namespace webapp
             {
               new JProperty("Name", "Result"),
               new JProperty("HttpVerb", "PUT"),
-              new JProperty("Resource", upload),
+              new JProperty("Resource", uploadToS3.GetLeftPart(UriPartial.Path)),
+              new JProperty("Headers", MakeHeaders(WebRequestMethods.Http.Put, uploadToS3)),
               new JProperty("StorageProvider",  "Generic")
             }
           }
         )
       };
 
-      string id = string.Empty;
-      dynamic workitem = await workItemsApi.CreateWorkItemAsync(new Autodesk.Forge.Model.WorkItem(id, arguments, null, null, null, "PlotToPDF"));
-      
+      // submit the workitem...
+      dynamic workitem = await workItemsApi.CreateWorkItemAsync(new Autodesk.Forge.Model.WorkItem(string.Empty, arguments, null, null, null, "PlotToPDF"));
+      // wait...
       System.Threading.Thread.Sleep(5000); // wait 1 second
+      // get the status
+      string id = workitem.Id;
+      dynamic status = await workItemsApi.GetWorkItemAsync(id);  // Due an issue with the .NET SDK, this is not working (#17)
 
-      id = workitem.Id;
-      dynamic status = await workItemsApi.GetWorkItemAsync(id);
-            
+      // download the PDF from S3 to our server
       fileSavePath = fileSavePath.Replace(".dwg", ".pdf");
-      client.DownloadToFilePath(BUCKET_NAME, pdfFileName, fileSavePath, null);
+      client.DownloadToFilePath(bucketName, pdfFileName, fileSavePath, null);
 
+      // send the PDF file to the client (DO NOT expose a direct URL to S3, but send the bytes)
       Response.Clear();
       Response.ContentType = "application/pdf";
       Response.AddHeader("Content-Disposition", "attachment;filename=\"" + pdfFileName + "\"");
@@ -98,14 +120,34 @@ namespace webapp
       Response.Flush();
       Response.End();
 
-      // delete file on S3
-      await client.DeleteObjectAsync(BUCKET_NAME, dwgFileName);
-      await client.DeleteObjectAsync(BUCKET_NAME, pdfFileName);
+      // delete files on S3
+      await client.DeleteObjectAsync(bucketName, dwgFileName);
+      await client.DeleteObjectAsync(bucketName, pdfFileName);
+      await client.DeleteBucketAsync(bucketName);
 
+      // delete PDF file from server
       Directory.Delete(Path.GetDirectoryName(fileSavePath), true);
+    }
 
-      // should be empty
-      IList<string> objectsInBucket = await client.GetAllObjectKeysAsync(BUCKET_NAME, string.Empty, null);
+    private JArray MakeHeaders(string verb, Uri query)
+    {
+      // Prepare headers
+      var collection = AWS.Signature.SignatureHeader(
+        Amazon.RegionEndpoint.USWest2, query.Host,
+        verb, query.AbsolutePath);
+
+      // organize headers for Design Automation
+      JArray headers = new JArray();
+      foreach (KeyValuePair<string, string> item in collection)
+      {
+        headers.Add(new JObject
+        {
+          new JProperty("Name", item.Key),
+          new JProperty("Value", item.Value)
+        });
+      }
+
+      return headers;
     }
   }
 }
